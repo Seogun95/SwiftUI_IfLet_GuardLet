887ad0d1a127cd66d3b637f8af4883fdb0e7fa1d
SeonJungKim <ksj083895@gmail.com>
2021-05-14 18:22:52 +0500

    Initial Commit

---
+++ Shared/SwiftUI_IfLet_GuardLetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_IfLet_GuardLetApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2021/05/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_IfLet_GuardLetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
